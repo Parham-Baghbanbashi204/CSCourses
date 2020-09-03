882a05f035214dc9f57f7af7fb9f3f9161bbbf9e
Parham Baghbanbashi <parham.baghbanbashi204@gmail.com>
2020-09-03 19:33:27 -0400

    added c++ tutorial

---
+++ cpptoutorial/cpptoutorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // preprocesser directive, tells the computer that you will be using a file, essantialy an import statment but unlike using namespace which imports a library 
+// include imports a file
+
+using namespace std; // using namespace imports a library like include but it imports a LIBRARY not a FILE
+// std stands for standard cpp LIBRARY
+
+// every program has a main function
+int cpptoutorial()
+{
+    //cout  = output stream object
+    //<<  = streem insertion operator
+    cout << "Hello World!" << endl;
+    //return = returns a value
+    //return 0 = no errors
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
---
+++ cpptoutorial/ooutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	//cout = tells the computer to print someting
+	// << = stream insertion operator anyting you put after this will be printed
+	// endl means end line same as \n
+	cout << "boy i love bacon" << endl;
+	cout << "and ham";
+
+	return 0;
+}
