2b13bf215b2114a6851129e65c3c2ebf8ed0ac74
Parham Baghbanbashi <parham.baghbanbashi204@gmail.com>
2020-10-05 19:49:08 -0400

    lists tuples and sets

--- CoryShaferPythonTutorials/working_with_numarical_data.py
+++ CoryShaferPythonTutorials/working_with_numarical_data.py
@@ -1,1 +1,33 @@
 # working with numarical data
+
+#integer whole number
+num = 3
+
+#float decimal number
+num_float = 3.3
+
+# Arithmetic Operators:
+# Addition:       3 + 2 -> 5
+# Subtraction:    3 - 2 -> 1
+# Multiplication: 3 * 2 -> 6
+# Division:       3 / 2 -> 1.5
+# Floor Division: 3 // 2 -> 1
+# Exponent:       3 ** 2 -> 9
+# Modulus:        3 % 2 -> 1 # gives the remainder
+# we can use parenthisis to do order of operations 
+
+
+# Comparisons:
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
+
+
+# Casting
+# num1 = '100'
+# num2 = '200'
+# print(num1 + num2) --> 100200
+# print(int(num1) + int(num2)) --> 300
